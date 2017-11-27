397868ae647a268bed9a2d0253688bbe8bdde2cc
Senol <msenolkeskin>
2017-11-27 15:02:46 +0300

    html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        filename: './dist/app.bundle.js'
-    }
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Perfect Website',
+      template: './src/index.html',
+      minify: {
+        // collapseWhitespace: true,
+      },
+    }),
+  ],
 };
