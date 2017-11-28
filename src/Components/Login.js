7fdb60b8c8929af02796b1f780248ab14c206935
Senol <msenolkeskin>
2017-11-28 17:49:29 +0300

    first component

---
+++ src/Components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => (
+  <div>
+    <h1>Please Login</h1>
+    <div>
+      <input type="text" placeholder="Type your user name" id="userName" />
+    </div>
+  </div>
+);
+
+export default Login;
--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Login from './Components/Login';
 
 require('./app.scss');
 
 const Component = () => (
   <div>
     <h1>Hello, world!</h1>
-    <input type="text" />
+    <Login />
   </div>
 );
 
