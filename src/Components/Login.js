73c4cf3707e07c02d6546a367871c1a87ac0af3e
Senol <msenolkeskin>
2017-12-05 17:52:45 +0300

    make submit button disabled

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,18 +1,39 @@
 import React from 'react';
 
-const Login = () => (
-  <div>
-    <h1>Please Login</h1>
-    <label htmlFor="userName">User name</label>
-    <input
-      type="text"
-      placeholder="Type your user name"
-      id="userName"
-      onFocus={e => e.target.select()}
-    />
-    <input type="password" placeholder="Password" id="password" onFocus={e => e.target.select()} />
-    <button>Submit</button>
-  </div>
-);
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      formIsValid: false,
+    };
+    this.submitHandle = evt => console.log(evt);
+  }
+
+  render() {
+    const title = `Please login ${this.state.formIsValid}`;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <label htmlFor="userName">User name</label>
+        <input
+          type="text"
+          placeholder="Type your user name"
+          id="userName"
+          onFocus={e => e.target.select()}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          onFocus={e => e.target.select()}
+        />
+        <button onClick={this.submitHandle} disabled={!this.state.formIsValid}>
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Login;
